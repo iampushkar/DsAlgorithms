2d2ecedf3e0537568efa1a8bb1a9a1c75474ef82
Manish Pushkar <m4manishpushkar@gmail.com>
2021-12-16 20:30:26 +0530

    initial commit

---
+++ Arrays/HelloWorld.java
@@ -0,0 +1,11 @@
+import javax.sound.sampled.SourceDataLine;
+
+/**
+ * HelloWorld
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
