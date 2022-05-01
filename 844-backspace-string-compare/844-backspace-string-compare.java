afc49958811d5348fddd88773d3aac4ac9a913f0
Manish Pushkar <m4manishpushkar@gmail.com>
2022-05-02 00:06:37 +0530

    Time: 4 ms (28.60%), Space: 42.6 MB (17.88%) - LeetHub

---
+++ 844-backspace-string-compare/844-backspace-string-compare.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean backspaceCompare(String s, String t) {
+        String s1 = delete(s);
+        String t1 = delete(t);
+        
+        if(s1.equals(t1)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
+    private String delete(String s) {
+        Stack<Character> st = new Stack<>();
+        for(char c : s.toCharArray()) {
+            if(!st.isEmpty() && c == '#') {
+                st.pop();
+            } else {
+                if(c != '#') {
+                    st.push(c);
+                }    
+            }
+        }
+        
+        StringBuilder sb = new StringBuilder();
+        while(!st.isEmpty()) {
+            sb.append(st.pop());
+        }
+        
+        return sb.reverse().toString();
+    }
+}
