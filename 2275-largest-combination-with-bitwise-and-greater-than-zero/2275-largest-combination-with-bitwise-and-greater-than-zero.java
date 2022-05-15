f69cac6e3f4502fabd00b89bac15def96be32a80
Manish Pushkar <m4manishpushkar@gmail.com>
2022-05-15 10:04:49 +0530

    Time: 46 ms (33.33%), Space: 72.1 MB (100.00%) - LeetHub

--- 2275-largest-combination-with-bitwise-and-greater-than-zero/2275-largest-combination-with-bitwise-and-greater-than-zero.java
+++ 2275-largest-combination-with-bitwise-and-greater-than-zero/2275-largest-combination-with-bitwise-and-greater-than-zero.java
@@ -3,6 +3,7 @@ class Solution {
         int max = 0, ans = 0;
         for(int i=31; i>=0; i--) {
             int count = 0;
+            
             for(int val : candidates) {
                 if(((val >> i) & 1) == 1) {
                     count++;
