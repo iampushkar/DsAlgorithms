119559aad84276cdbaf60da08b792e49acdb958d
Manish Pushkar <m4manishpushkar@gmail.com>
2024-06-23 18:35:03 +0530

    Time: 0 ms (100%), Space: 40.2 MB (38.82%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,17 +1,16 @@
 class Solution {
     public int climbStairs(int n) {
-        int[] dp = new int[n+1];
-        Arrays.fill(dp, -1);
-        return solve(n, dp);
-    }
-
-    private int solve(int n, int[] dp) {
         // base case
         if (n == 1 || n == 0) return 1;
 
-        if (dp[n] != -1) return dp[n];
-
-        dp[n] = solve(n-1, dp) + solve(n-2, dp);
+        int[] dp = new int[n+1];
+        Arrays.fill(dp, -1);
+        
+        dp[0] = dp[1] = 1;
+        for(int i=2; i<=n; i++) {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        
         return dp[n];
     }
 }
