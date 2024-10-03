320f11cd1293aaff80efc37b4fdf4082bf3c24e2
Manish Pushkar <m4manishpushkar@gmail.com>
2024-10-03 11:14:13 +0530

    Time: 1 ms (100%), Space: 42.3 MB (20.68%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,32 +1,54 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int length = nums.length;
-        int low = 0;
-        int high = length - 1;
-
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-
-            if (nums[mid] == target) return mid;
-
-            // Left half is sorted
-            if (nums[low] <= nums[mid]) {
-                if (nums[low] <= target && target < nums[mid]) {
-                    high = mid - 1;
-                } else {
-                    low = mid + 1;
-                }
-            } 
-            // Right half is sorted
-            else {
-                if (nums[mid] < target && target <= nums[high]) {
-                    low = mid + 1;
-                } else {
-                    high = mid - 1;
-                }
+        int N = nums.length;
+        int k = findRotationFactor(nums);
+        if(k == 0) { k = N; }
+        
+        if(target == nums[0]) {
+            return 0;
+        } else if(target > nums[0]) {
+            //we have to search in 1st array - [0, k-1]
+            return binarySearch(nums, target, 0, k-1);
+        } else {
+            //we have to search in 2nd sorted array - [k, N-1]
+            return binarySearch(nums, target, k, N-1);
+        }
+    }
+    
+    private static int binarySearch(int[] arr, int target, int start, int end) {
+        while(start <= end) {
+            int mid = (start+end)/2;
+            if(arr[mid] == target) {
+                return mid;
+            } else if(arr[mid] > target) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
             }
         }
-        
-        return -1; // Target not found
+        return -1;
+    }
+    
+    private static int findRotationFactor(int[] arr) {
+        int N = arr.length;
+        int start = 0, end = N-1;
+        int k = 0; //rotation factor
+        while(start <= end) {
+            int mid = (start + end) / 2;
+            if(arr[mid] < arr[0]) {
+                //we are at 2nd array
+                k = mid;
+                end = mid - 1;
+            } else {
+                //we are at 1st array
+                start = mid + 1;
+            }
+        }
+        return k;
     }
 }
+/*
+1. find the rotation factor - k
+2. based on condition split the array into 2 sorted arra : [0, k-1] or [k, N-1]
+3. apply binary search depending on the condition
+*/
