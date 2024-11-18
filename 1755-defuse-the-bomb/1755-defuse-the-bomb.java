7bda613431d63eff17e52bb67904b06a44051309
Manish Pushkar <m4manishpushkar@gmail.com>
2024-11-18 22:39:34 +0530

    Time: 150 ms (6.24%), Space: 45.7 MB (8.63%) - LeetHub

--- 1755-defuse-the-bomb/1755-defuse-the-bomb.java
+++ 1755-defuse-the-bomb/1755-defuse-the-bomb.java
@@ -1,26 +1,25 @@
 class Solution {
     public int[] decrypt(int[] code, int k) {
-        int[] ans = new int[code.length];
-         if (k == 0) {
-            return ans;
+        int N = code.length;
+        int[] res = new int[N];
+
+        if (k == 0) {
+            return res;
         }
-        if(k > 0){
-        for(int i=0;i<code.length;i++){
-            int sum = 0;
-            for(int j=1;j<=k;j++){
-                sum += code[(i+j) % code.length];
-            }
-            ans[i] = sum;
-            }
-        } else {
-            for(int i=0;i<code.length;i++){
-                int sum = 0;
-                for(int j=1;j<=-1 * k;j++){
-                    sum += code[(i-j + code.length) % code.length];
+
+        for (int i = 0; i < N; i++) {
+            if (k > 0) {
+                for (int j = i + 1; j < i + 1 + k; j++) {
+                    res[i] += code[j % N];
                 }
-                ans[i] = sum;
+            } else if (k < 0) {
+                for (int j = i - 1; j > i - 1 - Math.abs(k); j--) {
+                    res[i] += code[Math.abs((j +  N) % N)];
+                    System.out.println(i + ":" + j + " => " + res[i]);
+                }
             }
         }
-        return ans;
+
+        return res;
     }
 }
