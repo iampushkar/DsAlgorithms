00e3b31dab51e0e6c35446735654bbaec1203305
Manish Pushkar <m4manishpushkar@gmail.com>
2025-01-30 13:50:14 +0530

    Time: 8 ms (36.3%), Space: 45.6 MB (28.35%) - LeetHub

--- 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.java
+++ 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.java
@@ -1,17 +1,17 @@
 class Solution {
     public int minFallingPathSum(int[][] matrix) {
-        int ROWS = matrix.length;
-        int COLS = matrix[0].length;
-        int[][] dp = new int[ROWS][COLS];
+        // int ROWS = matrix.length;
+        // int COLS = matrix[0].length;
+        // int[][] dp = new int[ROWS][COLS];
 
-        for (int[] row : dp) {
-            Arrays.fill(row, -1);
-        }
+        // for (int[] row : dp) {
+        //     Arrays.fill(row, -1);
+        // }
 
         // int res = Integer.MAX_VALUE;
         // for (int c = 0; c < COLS; c++) {
-        //     int pathSum = solve(ROWS - 1, c, matrix, dp);
-        //     res = Math.min(res, pathSum);
+        // int pathSum = solve(ROWS - 1, c, matrix, dp);
+        // res = Math.min(res, pathSum);
         // }
 
         // return res;
@@ -20,38 +20,59 @@ class Solution {
 
     private int solve(int row, int col, int[][] matrix, int[][] dp) {
         // base case
-        if (row == 0) return matrix[0][col];
-        if (col < 0 || col >= matrix[0].length) return (int)Math.pow(10, 9);
+        if (row == 0)
+            return matrix[0][col];
+        if (col < 0 || col >= matrix[0].length)
+            return (int) Math.pow(10, 9);
 
-        if (dp[row][col] != -1) return dp[row][col];
+        if (dp[row][col] != -1)
+            return dp[row][col];
 
         int up = solve(row - 1, col, matrix, dp);
-        int leftUpDiagonal = (col > 0) ? solve(row - 1, col - 1, matrix, dp) : (int)Math.pow(10, 9);
-        int rightUpDiagonal = (col < matrix[0].length - 1) ? solve(row - 1, col + 1, matrix, dp) : (int)Math.pow(10, 9);
+        int leftUpDiagonal = (col > 0) ? solve(row - 1, col - 1, matrix, dp) : (int) Math.pow(10, 9);
+        int rightUpDiagonal = (col < matrix[0].length - 1) ? solve(row - 1, col + 1, matrix, dp)
+                : (int) Math.pow(10, 9);
 
-        int min = matrix[row][col]  + Math.min(up, Math.min(leftUpDiagonal, rightUpDiagonal));
+        int min = matrix[row][col] + Math.min(up, Math.min(leftUpDiagonal, rightUpDiagonal));
         return dp[row][col] = min;
     }
 
-    public int tabulation(int[][] matrix){
+    public int tabulation(int[][] matrix) {
         int n = matrix.length;
         int[][] dp = new int[n][n];
         int ans = Integer.MAX_VALUE;
-        for(int j = 0; j < n; j++) dp[0][j] = matrix[0][j];
-        for(int i = 1; i < n; i++){
-            for(int j = 0; j < n; j++){
-                int diag1 = 0, diag2 = 0;
-                int up = matrix[i][j] + dp[i - 1][j];
-                if(j > 0) diag1 = matrix[i][j] + dp[i - 1][j - 1];
-                else diag1 = (int)1e9;
-                if(j + 1 < n) diag2 = matrix[i][j] + dp[i - 1][j + 1];
-                else diag2 = (int)1e9;
-                int minPath = Math.min(up, Math.min(diag1, diag2));
+
+        for (int j = 0; j < n; j++)
+            dp[0][j] = matrix[0][j];
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                int leftUpDiagonal = 0, rightUpDiagonal = 0;
+
+                int up = dp[i - 1][j];
+
+                leftUpDiagonal = (j > 0) ?  dp[i - 1][j - 1] : (int) Math.pow(10, 9);
+
+                rightUpDiagonal = (j < n - 1) ? dp[i - 1][j + 1] : (int) Math.pow(10, 9);
+
+                // if (j > 0)
+                //     leftUpDiagonal = matrix[i][j] + dp[i - 1][j - 1];
+                // else
+                //     leftUpDiagonal = (int) 1e9;
+
+                // if (j + 1 < n)
+                //     diag2 = matrix[i][j] + dp[i - 1][j + 1];
+                // else
+                //     diag2 = (int) 1e9;
+
+                int minPath = matrix[i][j] + Math.min(up, Math.min(leftUpDiagonal, rightUpDiagonal));
                 dp[i][j] = minPath;
             }
         }
-        for(int j = 0; j < n; j++) ans = Math.min(ans, dp[n - 1][j]);
+
+        for (int j = 0; j < n; j++)
+            ans = Math.min(ans, dp[n - 1][j]);
+
         return ans;
     }
 }
-
