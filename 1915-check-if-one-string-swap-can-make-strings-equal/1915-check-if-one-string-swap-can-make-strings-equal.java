f3c1363014917cb13e1ee26a7561d78162cc05f4
Manish Pushkar <m4manishpushkar@gmail.com>
2025-02-06 00:46:28 +0530

    Time: 0 ms (100%), Space: 41.8 MB (39.72%) - LeetHub

--- 1915-check-if-one-string-swap-can-make-strings-equal/1915-check-if-one-string-swap-can-make-strings-equal.java
+++ 1915-check-if-one-string-swap-can-make-strings-equal/1915-check-if-one-string-swap-can-make-strings-equal.java
@@ -1,27 +1,19 @@
 class Solution {
     public boolean areAlmostEqual(String s1, String s2) {
-        if (s1.equals(s2)) {
-            return true;
-        }
+        if (s1.equals(s2)) return true;
 
         List<Integer> idxs = new ArrayList<>();
-        for (int i = 0; i < s1.length(); i++) {
-            if (s1.charAt(i) != s2.charAt(i)) {
-                idxs.add(i);
-            }
 
-            if (idxs.size() > 2) {
-                return false;
-            }
+        for (int i = 0; i < s1.length(); i++) {  
+            if (s1.charAt(i) != s2.charAt(i)) 
+                idxs.add(i);  
+                
+            if (idxs.size() > 2) 
+                return false;  
         }
 
-        if (idxs.size() == 2) {
-            int i = idxs.get(0);
-            int j = idxs.get(1);
-
-            return  s1.charAt(i) == s2.charAt(j) && s1.charAt(j) == s2.charAt(i);
-        }
-
-        return (idxs.size() == 0);
-    }
+        return idxs.size() == 2 &&  
+               s1.charAt(idxs.get(0)) == s2.charAt(idxs.get(1)) &&  
+               s1.charAt(idxs.get(1)) == s2.charAt(idxs.get(0));  
+    }  
 }
